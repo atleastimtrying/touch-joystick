843b4de48151fdedde07f155b4f9c4ece4240d7c
Anders Fisher <atleastimtrying@gmail.com>
2012-01-01 19:32:00 +0000

    still not working boo

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,24 @@
 (function() {
-
+  var makeHandle = function(element){
+    var mouseIsDown = false;
+    element.onmousedown = function(){
+      mouseIsDown = true;
+      console.log(element.style.left);
+    }
+    element.onmouseup = function(){
+      mouseIsDown = false;
+      console.log(element.style.left);
+    }
+    return{
+      element:element
+    }
+  }
+  var handles = '';
   window.onload = function() {
-    alert('hello');
+    handles = document.getElementsByClassName('handle');
+    for (var i = 0; i < handles.length; i++){
+      handles[i]
+    }
   };
 
 }).call(this);
